f15e51f90a04d9ff528189aa49f43fde372e76ea
hottah <info@hottah.jp>
2026-01-13 14:34:28 +0900

    情報更新：制作事例
    1件公開のため情報を差し替え

--- src/pages/works.js
+++ src/pages/works.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const works = [
   { title: 'ブランドサイト', url: 'https://hanaemu.co.jp', desc: 'デザインディレクターより受注：Figma構築案件。' },
-  { title: 'NPO法人リニューアル案', url: 'https://npo-tagayasu.o.oo7.jp/_next/',  desc: '個人受注：要件定義から完成まで。（公開待ち）' },
+  { title: 'NPO法人リニューアル案', url: 'https://npo-tagayasu.o.oo7.jp/',  desc: '個人受注：要件定義から完成まで。' },
 ];
 
 export default function Works() {
